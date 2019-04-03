b4fd75e4792c22c7fcc3f5d1a1a0df9027615f2c
Samuel <jread912@gmail.com>
2019-04-03 11:41:55 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage/Welcome/Welcome';
+import LearnerLogin from './components/Learner/Login/Login';
+import LearnerRegister from './components/Learner/Register/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          <Route exact path="/login" component={LearnerLogin} />
+          <Route exact path="/register" component={LearnerRegister} />
+        </Switch>
+      </Router>
     );
   }
 }
